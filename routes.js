8ecb7b72a6e13f55f4e2bf552a221a87204062f4
Leonardo Campello <leonardocampello.dev@gmail.com>
2020-06-30 22:16:40 -0300

    Receiving data from the front end

--- routes.js
+++ routes.js
@@ -4,15 +4,23 @@ const { Router } = require('express')
 const routes = express.Router()
 
 routes.get("/", (req, res) => {
-    res.redirect("instructors")
+    return res.redirect("instructors")
 })
 
 routes.get("/instructors", (req, res) => {
-    res.render("instructors/index")
+    return res.render("instructors/index")
+})
+
+routes.get("/instructors/create", (req, res) => {
+    return res.render("instructors/create")
+})
+
+routes.post("/instructors", (req, res) => {
+    return res.send(req.body)
 })
 
 routes.get("/members", (req, res) => {
-    res.render("members")
+    return res.render("members")
 })
 
 module.exports = routes
