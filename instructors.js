ebf54cb4c0f70bd5f31b859be3d6d7ebdcd4ce34
Leonardo Campello <leonardocampello.dev@gmail.com>
2020-07-06 18:47:56 -0300

    Listing instructors

--- instructors.js
+++ instructors.js
@@ -2,6 +2,9 @@ const fs = require('fs')
 const data = require('./data')
 const { age, date } = require('./utils')
 
+exports.index = (req, res) => {    
+    return res.render("instructors/index", { instructors: data.instructors })
+}
 
 exports.show = (req, res) => {
     const { id } = req.params
@@ -87,7 +90,8 @@ exports.put = (req, res) => {
     const instructor = {
         ...foundInstructor,
         ...req.body,
-        birth: Date.parse(req.body.birth)
+        birth: Date.parse(req.body.birth),
+        id: Number(req.body.id)
     }
 
     data.instructors[index] = instructor
